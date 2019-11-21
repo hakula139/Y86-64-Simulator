7642c19f9797d659e1e32c26b821cd50816be5fb
Hakula Chen <i@hakula.xyz>
2019-11-21 18:58:53 +0800

    Implement functions in ALU

--- src/assets/alu.cpp
+++ src/assets/alu.cpp
@@ -6,17 +6,85 @@ namespace assets {
 
 int64_t ArithmeticLogicUnit::AddSigned(int64_t num1, int64_t num2) {
     int64_t result = num1 + num2;
+    // signed overflow
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(SF, result < 0);
     ConditionCode::Set(OF, (num1 > 0 && num2 > 0 && result < 0) ||
                                (num1 < 0 && num2 < 0 && result >= 0));
-    ConditionCode::Set(SF, result < 0);
-    ConditionCode::Set(ZF, !result);
     return result;
 }
 
 uint64_t ArithmeticLogicUnit::AddUnsigned(uint64_t num1, uint64_t num2) {
     uint64_t result = num1 + num2;
-    ConditionCode::Set(CF, result < num1 && result < num2);
+    // unsigned overflow
     ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(CF, result < num1 && result < num2);
+    return result;
+}
+
+int64_t ArithmeticLogicUnit::SubSigned(int64_t num1, int64_t num2) {
+    return AddSigned(num1, -num2);
+}
+
+uint64_t ArithmeticLogicUnit::SubUnsigned(uint64_t num1, uint64_t num2) {
+    return AddUnsigned(num1, -num2);
+}
+
+uint64_t ArithmeticLogicUnit::Inc(uint64_t num) { return AddUnsigned(num, 1); }
+
+uint64_t ArithmeticLogicUnit::Dec(uint64_t num) { return SubUnsigned(num, 1); }
+
+int64_t ArithmeticLogicUnit::And(int64_t num1, int64_t num2) {
+    int64_t result = num1 & num2;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(SF, result < 0);
+    ConditionCode::Set(OF, 0);
+    return result;
+}
+
+int64_t ArithmeticLogicUnit::Or(int64_t num1, int64_t num2) {
+    int64_t result = num1 | num2;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(SF, result < 0);
+    ConditionCode::Set(OF, 0);
+    return result;
+}
+
+int64_t ArithmeticLogicUnit::Xor(int64_t num1, int64_t num2) {
+    int64_t result = num1 ^ num2;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(SF, result < 0);
+    ConditionCode::Set(OF, 0);
+    return result;
+}
+
+int64_t ArithmeticLogicUnit::ArithmeticLeftShift(int64_t num, size_t count) {
+    int64_t result = num << count;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(SF, result < 0);
+    ConditionCode::Set(OF, (num < 0 && result >= 0) || (num > 0 && result < 0));
+    return result;
+}
+
+int64_t ArithmeticLogicUnit::ArithmeticRightShift(int64_t num, size_t count) {
+    int64_t result = num >> count;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(SF, result < 0);
+    ConditionCode::Set(OF, 0);
+    return result;
+}
+
+uint64_t ArithmeticLogicUnit::LogicalLeftShift(uint64_t num, size_t count) {
+    uint64_t result = num << count;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(CF, result < num);
+    return result;
+}
+
+uint64_t ArithmeticLogicUnit::LogicalRightShift(uint64_t num, size_t count) {
+    uint64_t result = num >> count;
+    ConditionCode::Set(ZF, !result);
+    ConditionCode::Set(CF, 0);
     return result;
 }
 
--- src/assets/alu.h
+++ src/assets/alu.h
@@ -5,11 +5,35 @@
 
 namespace assets {
 
+// Provides basic operations for integers and sets condition codes respectively
 class ArithmeticLogicUnit {
 public:
-    static int64_t  AddSigned(int64_t num1, int64_t num2);
+    // Adds 'num2' to 'num1' (both are signed)
+    static int64_t AddSigned(int64_t num1, int64_t num2);
+    // Adds 'num2' to 'num1' (both are unsigned)
     static uint64_t AddUnsigned(uint64_t num1, uint64_t num2);
-    // TODO(Hakula): Add more operations
+    // Subtracts 'num2' from 'num1' (both are signed)
+    static int64_t SubSigned(int64_t num1, int64_t num2);
+    // Subtracts 'num2' from 'num1' (both are unsigned)
+    static uint64_t SubUnsigned(uint64_t num1, uint64_t num2);
+    // Increases 'num' by 1 (unsigned)
+    static uint64_t Inc(uint64_t num);
+    // Decreases 'num' by 1 (unsigned)
+    static uint64_t Dec(uint64_t num);
+    // Does the bitwise AND on 'num1' and 'num2' (signed)
+    static int64_t And(int64_t num1, int64_t num2);
+    // Does the bitwise OR on 'num1' and 'num2' (signed)
+    static int64_t Or(int64_t num1, int64_t num2);
+    // Does the bitwise XOR on 'num1' and 'num2' (signed)
+    static int64_t Xor(int64_t num1, int64_t num2);
+    // Left-shifts 'num' by 'count' bits (signed)
+    static int64_t ArithmeticLeftShift(int64_t num, size_t count);
+    // Right-shifts 'num' by 'count' bits (signed)
+    static int64_t ArithmeticRightShift(int64_t num, size_t count);
+    // Left-shifts 'num' by 'count' bits (unsigned)
+    static uint64_t LogicalLeftShift(uint64_t num, size_t count);
+    // Right-shifts 'num' by 'count' bits (unsigned)
+    static uint64_t LogicalRightShift(uint64_t num, size_t count);
 };
 
 }  // namespace assets
