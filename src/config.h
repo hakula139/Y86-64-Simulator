421816b57b94e25012732a149dcaa49e0b4c1f5f
Hakula Chen <i@hakula.xyz>
2019-11-21 19:05:18 +0800

    Revert "Update version"
    This reverts commit 4af93b3473140c95d62de443f2e5761f3ffdc9aa.

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define SRC_CONFIG_H_
 
 constexpr int kSimVersionMajor = 0;
-constexpr int kSimVersionMinor = 2;
+constexpr int kSimVersionMinor = 1;
 constexpr int kSimVersionPatch = 0;
 
 #endif  // SRC_CONFIG_H_
