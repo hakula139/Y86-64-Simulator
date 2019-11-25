c3438efcfa426951aeb714e5d76afcbfbbbabb39
Hakula Chen <i@hakula.xyz>
2019-11-26 01:07:40 +0800

    Update Register

--- src/assets/register.cpp
+++ src/assets/register.cpp
@@ -3,7 +3,6 @@
 namespace assets {
 
 std::vector<uint64_t> Register::data_(kTotal_);
-std::vector<bool>     ConditionCode::data_(kTotal_);
 
 bool Register::Set(int register_num, uint64_t value) {
     data_.at(register_num) = value;
@@ -11,16 +10,6 @@ bool Register::Set(int register_num, uint64_t value) {
 }
 
 bool Register::Clear() {
-    for (auto&& value : data_) value = 0;
-    return true;
-}
-
-bool ConditionCode::Set(int condition_code, bool value) {
-    data_.at(condition_code) = value;
-    return true;
-}
-
-bool ConditionCode::Clear() {
     for (auto&& value : data_) value = 0;
     return true;
 }
--- src/assets/register.h
+++ src/assets/register.h
@@ -7,22 +7,41 @@
 namespace assets {
 
 enum RegisterMap : int {
-    RAX = 0x0,
-    RCX = 0x1,
-    RDX = 0x2,
-    RBX = 0x3,
-    RSP = 0x4,
-    RBP = 0x5,
-    RSI = 0x6,
-    RDI = 0x7,
-    R8  = 0x8,
-    R9  = 0x9,
-    R10 = 0xA,
-    R11 = 0xB,
-    R12 = 0xC,
-    R13 = 0xD,
-    R14 = 0xE,
-    R15 = 0xF
+    RAX   = 0x0,
+    RCX   = 0x1,
+    RDX   = 0x2,
+    RBX   = 0x3,
+    RSP   = 0x4,
+    RBP   = 0x5,
+    RSI   = 0x6,
+    RDI   = 0x7,
+    R8    = 0x8,
+    R9    = 0x9,
+    R10   = 0xA,
+    R11   = 0xB,
+    R12   = 0xC,
+    R13   = 0xD,
+    R14   = 0xE,
+    RNONE = 0xF
+};
+
+enum PipelineRegisterMap : int {
+    PRED_PC = 0x0,
+    STAT    = 0x1,
+    I_CODE  = 0x2,
+    I_FUN   = 0x3,
+    R_A     = 0x4,
+    R_B     = 0x5,
+    VAL_C   = 0x6,
+    VAL_P   = 0x7,
+    VAL_A   = 0x8,
+    VAL_B   = 0x9,
+    VAL_E   = 0xA,
+    VAL_M   = 0xB,
+    SRC_A   = 0xC,
+    SRC_B   = 0xD,
+    DST_E   = 0xE,
+    DST_M   = 0xF
 };
 
 enum ConditionCodeMap : int { OF = 0x0, SF = 0x1, ZF = 0x2, CF = 0x3 };
@@ -39,16 +58,16 @@ protected:
     static std::vector<uint64_t> data_;
 };
 
-// Manages the value in each condition code
-class ConditionCode {
-public:
-    static bool Get(int condition_code) { return data_.at(condition_code); }
-    static bool Set(int condition_code, bool value);
-    static bool Clear();
-
+// Manages the value in each pipeline register
+class PipelineRegister : public Register {
 protected:
-    static constexpr size_t  kTotal_ = 3;
-    static std::vector<bool> data_;
+    static constexpr size_t kTotal_ = 16;
+};
+
+// Manages the value in each condition code
+class ConditionCode : public Register {
+protected:
+    static constexpr size_t kTotal_ = 3;
 };
 
 }  // namespace assets
