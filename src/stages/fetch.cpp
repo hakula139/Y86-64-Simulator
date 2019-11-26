86dc0b21fa03f880102171bf598be2731f6c21e6
Hakula Chen <i@hakula.xyz>
2019-11-26 16:59:37 +0800

    Fetch stage current progress

--- src/stages/fetch.cpp
+++ src/stages/fetch.cpp
@@ -1,7 +1,54 @@
 #include "fetch.h"
 
+#include <iostream>
+
+#include "../assets/file.h"
+#include "../assets/register.h"
+#include "../utils/utility.h"
+#include "instruction.h"
+
 namespace stages {
 
-bool Fetch::InstructionIsValid() {}
+bool Fetch::Do(const assets::File& input) {
+    auto pc      = assets::ProgramCounter::Get();
+    instruction_ = input.GetInstruction(pc);
+    auto icode   = GetICode();
+    if (!InstructionIsValid(icode)) {
+        Fetch::PrintErrorMessage(2);
+        return false;
+    }
+    auto ifun = GetIFun();
+    return true;
+}
+
+uint8_t Fetch::GetICode() {
+    if (instruction_.empty()) {
+        Fetch::PrintErrorMessage(1);
+        return INOP;
+    }
+    return (instruction_[0] >> 4) & 0xF;  // the first 4 bits
+}
+
+uint8_t Fetch::GetIFun() {
+    if (instruction_.empty()) return FNONE;
+    return instruction_[0] & 0xF;  // the second 4 bits
+}
+
+bool Fetch::InstructionIsValid(uint8_t icode) {
+    std::vector<uint8_t> valid_icodes{IHALT,   INOP,    IRRMOVQ, IIRMOVQ,
+                                      IRMMOVQ, IMRMOVQ, IOPQ,    IJXX,
+                                      ICALL,   IRET,    IPUSHQ,  IPOPQ};
+    return utility::ValueIsInArray(icode, valid_icodes);
+}
+
+bool Fetch::PrintErrorMessage(const int error_code) {
+    std::cerr << "Fetch Error ";
+    switch (error_code) {
+        case 1: std::cerr << "1: No instruction.\n"; break;
+        case 2: std::cerr << "2: Invalid instruction.\n"; break;
+        default: std::cerr << "X: An unknown error occurs.\n"; break;
+    }
+    return true;
+}
 
 }  // namespace stages
--- src/stages/fetch.h
+++ src/stages/fetch.h
@@ -1,15 +1,25 @@
 #ifndef SRC_STAGES_FETCH_H_
 #define SRC_STAGES_FETCH_H_
 
+#include <cstdint>
+#include <vector>
+
+#include "../assets/file.h"
 #include "../assets/register.h"
 
 namespace stages {
 
 class Fetch {
 public:
-    static bool InstructionIsValid();
+    static bool    Do(const assets::File& input);
+    static uint8_t GetICode();
+    static uint8_t GetIFun();
+    static bool    InstructionIsValid(uint8_t icode);
 
 protected:
+    static bool PrintErrorMessage(const int error_code);
+
+    static std::vector<uint8_t>     instruction_;
     static assets::PipelineRegister register_;
 };
 
