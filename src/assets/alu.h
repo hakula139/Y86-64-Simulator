a1a6b77035caee02228e55b3fe8d99bf8b51abb6
Hakula Chen <i@hakula.xyz>
2019-11-27 01:01:40 +0800

    Update ALU (remove unnecessary functions)

--- src/assets/alu.h
+++ src/assets/alu.h
@@ -5,35 +5,32 @@
 
 namespace assets {
 
+enum AluFunctionMap : int { ADDQ = 0x0, SUBQ = 0x1, ANDQ = 0x2, XORQ = 0x3 };
+
+enum ConditionMap : int {
+    C_AW = 0x0,
+    C_LE = 0x1,
+    C_L  = 0x2,
+    C_E  = 0x3,
+    C_NE = 0x4,
+    C_GE = 0x5,
+    C_G  = 0x6
+};
+
 // Provides basic operations for integers and sets condition codes respectively
 class ArithmeticLogicUnit {
 public:
-    // Adds 'num2' to 'num1' (both are signed)
-    static int64_t AddSigned(int64_t num1, int64_t num2);
-    // Adds 'num2' to 'num1' (both are unsigned)
-    static uint64_t AddUnsigned(uint64_t num1, uint64_t num2);
-    // Subtracts 'num2' from 'num1' (both are signed)
-    static int64_t SubSigned(int64_t num1, int64_t num2);
-    // Subtracts 'num2' from 'num1' (both are unsigned)
-    static uint64_t SubUnsigned(uint64_t num1, uint64_t num2);
-    // Increases 'num' by 1 (unsigned)
-    static uint64_t Inc(uint64_t num);
-    // Decreases 'num' by 1 (unsigned)
-    static uint64_t Dec(uint64_t num);
-    // Does the bitwise AND on 'num1' and 'num2' (signed)
+    // Adds 'num2' to 'num1'
+    static int64_t Add(int64_t num1, int64_t num2);
+    // Subtracts 'num2' from 'num1'
+    static int64_t Sub(int64_t num1, int64_t num2);
+    // Does the bitwise AND on 'num1' and 'num2'
     static int64_t And(int64_t num1, int64_t num2);
-    // Does the bitwise OR on 'num1' and 'num2' (signed)
-    static int64_t Or(int64_t num1, int64_t num2);
-    // Does the bitwise XOR on 'num1' and 'num2' (signed)
+    // Does the bitwise XOR on 'num1' and 'num2'
     static int64_t Xor(int64_t num1, int64_t num2);
-    // Left-shifts 'num' by 'count' bits (signed)
-    static int64_t ArithmeticLeftShift(int64_t num, size_t count);
-    // Right-shifts 'num' by 'count' bits (signed)
-    static int64_t ArithmeticRightShift(int64_t num, size_t count);
-    // Left-shifts 'num' by 'count' bits (unsigned)
-    static uint64_t LogicalLeftShift(uint64_t num, size_t count);
-    // Right-shifts 'num' by 'count' bits (unsigned)
-    static uint64_t LogicalRightShift(uint64_t num, size_t count);
+
+protected:
+    static bool SetConditionCode(bool zf, bool sf, bool of);
 };
 
 }  // namespace assets
