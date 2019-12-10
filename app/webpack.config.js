672fc90800de76bf4c405e576d8f2542744c4ca3
Hakula Chen <i@hakula.xyz>
2019-12-11 01:37:35 +0800

    Init

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,8 +1,87 @@
-const path = require('path')
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const NodeExternals = require('webpack-node-externals');
+
+const rootPath = path.resolve(__dirname);
+const srcPath = path.resolve(rootPath, 'src');
+const distPath = path.resolve(rootPath, 'dist');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: "bundle.js"
-    }
-}
+  entry: srcPath,
+  output: {
+    path: distPath,
+    filename: 'bundle.js'
+  },
+  target: 'node',
+  externals: [NodeExternals()],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + '/';
+              }
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader'
+        }]
+      },
+      {
+        test: /\.(eot|woff2?|ttf|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: '[name].[hash:5].[ext]',
+            limit: 3000,
+            publicPath: "font/",
+            outputPath: "font/"
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: '[name].[hash:5].[ext]',
+            limit: 10000,
+            publicPath: "img/",
+            outputPath: "img/"
+          }
+        }]
+      }
+    ]
+  }
+};
