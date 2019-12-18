800c0d9bf6ec014bbea0e06b34e90470d401e662
Hakula Chen <i@hakula.xyz>
2019-12-18 21:16:20 +0800

    Update version to 4.0.0

--- lib/src/config.h
+++ lib/src/config.h
@@ -4,8 +4,8 @@
 #define SIM_DEBUG 0
 #define HAS_GUI 1
 
-constexpr int kSimVersionMajor = 3;
-constexpr int kSimVersionMinor = 3;
+constexpr int kSimVersionMajor = 4;
+constexpr int kSimVersionMinor = 0;
 constexpr int kSimVersionPatch = 0;
 
 #endif  // SRC_CONFIG_H_
